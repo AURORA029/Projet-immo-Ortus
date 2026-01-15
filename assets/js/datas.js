f90f72a04d34355f19d6439189199c413885359c
AURORA029 <allan.rafidison@gmail.com>
2026-01-15 21:17:25 +0100

    Create datas.js

---
+++ assets/js/datas.js
@@ -0,0 +1,38 @@
+const biensImmobiliers = [
+    {
+        id: 1,
+        titre: "Bungalow tropical - 25m²",
+        prix: "4 500 000 Ar",
+        ville: "Mahajanga, Bord de mer",
+        image: "assets/img/maison1.jpg", // Vérifie que tes images s'appellent bien comme ça
+        badge: "Vente",
+        lits: 4,
+        bains: 2,
+        surface: "25m²",
+        description: "Un magnifique bungalow les pieds dans l'eau. Idéal pour les vacances ou un investissement locatif rentable. Vue imprenable sur le canal du Mozambique."
+    },
+    {
+        id: 2,
+        titre: "Maison traditionnelle",
+        prix: "1 000 000 Ar /mois",
+        ville: "Centre Village, Bongatsara",
+        image: "assets/img/maison2.jpg",
+        badge: "Location",
+        lits: 2,
+        bains: 1,
+        surface: "85m²",
+        description: "Authenticité et calme pour cette maison traditionnelle en terres rouges, rénovée avec goût. Proche de toutes commodités locales."
+    },
+    {
+        id: 3,
+        titre: "Maison à rénover",
+        prix: "85 000 000 Ar",
+        ville: "Faravohitra",
+        image: "assets/img/maison3.jpg",
+        badge: "Vente",
+        lits: 0,
+        bains: 0,
+        surface: "1200m²",
+        description: "Opportunité rare ! Potentiel énorme pour ce bâtiment historique situé dans le quartier noble de la haute ville. Terrain titré et borné."
+    }
+];
