5b6ab6f791643c3bdaebb09e03a0654d160e7476
AURORA029 <allan.rafidison@gmail.com>
2026-01-16 03:20:52 +0100

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,60 @@
+const biensImmobiliers = [
+    {
+        id: 1,
+        titre: "Bungalow tropical - 25m²",
+        prix: "4 500 000 Ar",
+        ville: "Mahajanga, Bord de mer",
+        badge: "Vente",
+        image: "assets/img/maison1.jpg",
+        lits: 4,
+        bains: 2,
+        surface: "25m²",
+        description: "Un magnifique bungalow les pieds dans l'eau. Idéal pour les vacances ou un investissement locatif rentable. Vue imprenable sur le canal du Mozambique.",
+        // C'est ici qu'on ajoute les caractéristiques uniques :
+        caracteristiques: [
+            "Vue Mer Panoramique",
+            "Accès Plage direct",
+            "Terrasse aménagée",
+            "Climatisation",
+            "Parking sécurisé"
+        ]
+    },
+    {
+        id: 2,
+        titre: "Maison traditionnelle en terres rouges",
+        prix: "1 000 000 Ar /mois",
+        ville: "Centre Village, Bongatsara",
+        badge: "Location",
+        image: "assets/img/maison2.jpg",
+        lits: 2,
+        bains: 1,
+        surface: "85m²",
+        description: "Authenticité et calme pour cette maison traditionnelle en terres rouges, rénovée avec goût. Proche de toutes commodités locales.",
+        caracteristiques: [
+            "Charme authentique",
+            "Jardin potager",
+            "Quartier calme",
+            "Proche école",
+            "Accès eau & électricité"
+        ]
+    },
+    {
+        id: 3,
+        titre: "Maison à renover",
+        prix: "85 000 000 Ar",
+        ville: "Faravohitra",
+        badge: "Vente",
+        image: "assets/img/maison3.jpg",
+        lits: 0,
+        bains: 0,
+        surface: "1200m²",
+        description: "Opportunité rare ! Potentiel énorme pour ce bâtiment historique situé dans le quartier noble de la haute ville. Terrain titré et borné.",
+        caracteristiques: [
+            "Terrain Titré & Borné",
+            "Zone constructible",
+            "Vue sur Tana",
+            "Bâtiment historique",
+            "Grande surface (1200m²)"
+        ]
+    }
+];
